c1ff86294a26fc7d24f828634d51711e69802f03
Daniel <arutyunyan.daniel23@mail.ru>
2017-12-20 17:17:33 +0300

    Class programm (use kestral) webHost Start

--- Lesson/Program.cs
+++ Lesson/Program.cs
@@ -7,14 +7,26 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.AspNetCore.Http;
 namespace Lesson
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+            using (var host = WebHost.Start("http://localhost:8080", context => context.Response.WriteAsync("Hello loshara!")))
+            {
+                Console.WriteLine("Apii started!");
+                host.WaitForShutdown();
+            } 
+            //var host = new WebHostBuilder()
+            //    .UseKestrel()
+            //    .UseContentRoot(Directory.GetCurrentDirectory())
+            //    .UseIISIntegration()
+            //    .UseStartup<Startup>()
+            //    .Build();
+            //host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
