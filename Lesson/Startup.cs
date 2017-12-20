7fb369600d20b6adee7a9a989f27d0806e74bd3b
Daniel <arutyunyan.daniel23@mail.ru>
2017-12-20 18:04:57 +0300

    class startup

--- Lesson/Startup.cs
+++ Lesson/Startup.cs
@@ -9,15 +9,14 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Lesson
 {
+    //обработка запросов
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        //добавление сервисов
         public void ConfigureServices(IServiceCollection services)
         {
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //метод определяет как запрос будет обрабатываться
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
